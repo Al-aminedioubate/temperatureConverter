9f4265512fda995d2a4438ad928a9ccafad54efa
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-01-05 22:20:54 -0500

    mise en place et creation d'interface

---
+++ tempConverter.js
@@ -0,0 +1,17 @@
+//Recuperation nos rentrer de donnees
+let tCelsius = document.getElementById("celsiusT");
+let tFahreneit = document.getElementById("fahreneitT");
+let tKalvinT = document.getElementById("kalvinT");
+
+let tempConv, temperature;
+//Let use swich statement to track which input we're working on
+switch(temperature){
+    case 1:
+        tempConv = tCelsius;
+        
+        break;
+    case tFahreneit:
+        break;
+    case tKalvinT:
+        break;
+}
