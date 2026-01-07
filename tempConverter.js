22ce8adc2846b47d1090f2470e2ddd5d365afc5c
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-01-06 23:35:42 -0500

    logique de calcul a revoir

--- tempConverter.js
+++ tempConverter.js
@@ -5,18 +5,19 @@ let tKalvin = document.getElementById("kalvinT");
 
 let tempC, temperature, tempF, tempK;
 //convertissons les valeurs dans les temperature respective
-tempC = Number(tCelsius.value);
-tempK = Number(tKalvin.value);
-tempF = Number(tFahreneit.value);
 
-tempC = 100;
-//Celsius en Fahreneit et en Kalvin
-let F = (tempC * 9) / 5 + 32;
-let K = tempC + 273.15;
-//Fahreneit en Celsius et en Kalvin
-let C2 = ((F - 32) * 5) / 9;
-let K2 = ((F - 32) * 5) / 9 + 273.15;
-//Fahreneit en Celsius et en Kalvin
+tCelsius.onchange = function () {
+	const value = tCelsius.value;
+	if (value === "") {
+		tFahreneit.textContent = "";
+		return;
+	}
+	const fahrenheit = (value * 9) / 5 + 32;
+
+	document.getElementById("resultat").textContent = `${fahrenheit.toFixed(2)} °F`;
+
+	console.log(tFahreneit);
+};
 
 //Let use swich statement to track which input we're working on
 switch (temperature) {
