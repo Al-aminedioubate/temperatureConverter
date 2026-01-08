9238051e44d819ad2ac62713b0224a33104a9a54
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-01-07 23:28:18 -0500

    kalvin in C and F calcul done

--- tempConverter.js
+++ tempConverter.js
@@ -15,11 +15,11 @@ tCelsius.onchange = function () {
 
 	//Calcul F
 	const fahrenheit = (value * 9) / 5 + 32;
-	tFahrenheit.value = `${fahrenheit.toFixed(2)} °F`;
+	tFahrenheit.value = `${fahrenheit.toFixed(2)}`;
 
 	//Calcul K
 	const kalvin = value + 273.15;
-	tKalvin.value = `${kalvin} °K`;
+	tKalvin.value = `${kalvin}`;
 };
 
 //Fahreneit en C et en Kalvin
@@ -27,16 +27,35 @@ tFahrenheit.onchange = function () {
 	const valueC = tFahrenheit.value;
 	if (valueC === "") {
 		tCelsius.textContent = "";
+		tKalvin.textContent = "";
 		return;
 	}
 
 	//calcul de C
 	const celsius = ((valueC - 32) * 5) / 9;
-	tCelsius.value = `${celsius.toFixed(2)} C°`;
+	tCelsius.value = `${celsius.toFixed(2)}`;
 
 	//calcul de K
 	const kalvin2 = ((valueC - 32) * 5) / 9 + 273.15;
-	tKalvin.value = `${kalvin2.toFixed(2)} K`;
+	tKalvin.value = `${kalvin2.toFixed(2)}`;
+};
+
+//Kalvin en C et F
+tKalvin.onchange = function () {
+	const valueK = tKalvin.value;
+	if (valueK === "") {
+		tCelsius.textContent = "";
+		tFahrenheit.textContent = "";
+		return;
+	}
+
+	//calcul C
+	const celsius = valueK - 273.15;
+	tCelsius.value = `${celsius.toFixed(2)}`;
+
+	//calcul de F
+	const f = ((valueK - 273.15) * 9) / 5 + 32;
+	tFahrenheit.value = `${f.toFixed(2)}`;
 };
 
 //Let use swich statement to track which input we're working on
